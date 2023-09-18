e0927b28bd3f86344c59fe146f3dea2a958cdcd3
Jorrit <vangilsjorrit@gmail.com>
2023-09-18 18:44:47 -0500

    supported locales

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -4,12 +4,21 @@
     {{ $i18n.locale }}
     <select v-model="$i18n.locale">
 
-      <option value="en">English</option>
-
-      <option value="ar">Arabic</option>
+      <option :value="locale.code" v-for="locale in locales" :key="locale.code">
+        {{locale.name}}
+      </option>
 
     </select>
 
   </div>
 
 </template>
+
+<script>
+
+import { getSupportedLocales } from '@/util/i18n/supported-locales'
+
+export default {
+  data: () => ({ locales: getSupportedLocales() })
+}
+</script>
